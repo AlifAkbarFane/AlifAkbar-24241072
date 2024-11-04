f70c1b82e39d13203e842be17fe3c80fb6b5bb63
AlifAkbarFane <alifa27kbar@gmail.com>
2024-11-04 14:54:03 +0800

    Delete BilGenapGanjil.py

--- BilGenapGanjil.py
+++
@@ -1,8 +0,0 @@
-# meminta input dari pengguna 
-number = int(input("masukan sebuah bilangan : "))
-
-#memeriksa apakah genap atau ganjil 
-if number % 2 == 0 : 
-    print (f"{number} adalah bilangan genap. ")
-else:
-    print(f"{number}adalah bilangan ganjil. ")
