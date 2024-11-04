fbf6489975cc96bf92589dd29b71c263d9474865
AlifAkbarFane <alifa27kbar@gmail.com>
2024-11-04 14:53:42 +0800

    Delete KonversiSuhu.py

--- KonversiSuhu.py
+++
@@ -1,8 +0,0 @@
-# Meminta input dari dalam fahrenheit
-fahrenheit = float(input("Masukkan temperatur dalam Fahrenheit: "))
-
-# Mengonversi Fahrenheit ke Celcius
-celsius = (fahrenheit - 32) * 5/9
-
-# Menampilkan hasil konversi 
-print(f"{fahrenheit}derajat Fahrenheit = {celsius:.2f}derajat Celcius")
